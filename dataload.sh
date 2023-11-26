a41ee52827ba3ac411c568871c77be838e33b76f
david-heurtel-depeiges <david.heurtel.depeiges@gmail.com>
2023-11-26 14:02:47 -0500

    cleaning

--- dataload.sh
+++
@@ -1,8 +0,0 @@
-cp -r /mnt/home/gkrawezik/ceph/AI_DATASETS/ImageNet/2012/imagenet /tmp/imagenet
-pushd /tmp/imagenet/
-mkdir val train
-cd train/
-unzip -qq ../train.zip
-cd ../val/
-unzip -qq ../val.zip
-popd
--- dataload_val_only.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p /tmp/imagenet
-cp -r /mnt/home/gkrawezik/ceph/AI_DATASETS/ImageNet/2012/imagenet/val.zip /tmp/imagenet/val.zip
-pushd /tmp/imagenet/
-mkdir val
-cd val/
-unzip -qq ../val.zip
-popd
-
